3772e9fc6d4de873891f13cca61e008b866d9bb0
555Message <zaku596@yahoo.co.jp>
2022-11-25 12:00:37 +0900

    create null1

---
+++ Null1.java
@@ -0,0 +1,8 @@
+public class Null1 {
+    public static void main(String[] args) {
+        String str1;
+
+        str1 = "あいう";
+        System.out.println(str1 + "の文字数" + str1.length());
+    }
+}
