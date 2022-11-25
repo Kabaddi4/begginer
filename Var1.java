53cfe0e546a75aa9a66725e101412d456328cbc9
555Message <zaku596@yahoo.co.jp>
2022-11-25 11:31:43 +0900

    create var1

---
+++ Var1.java
@@ -0,0 +1,20 @@
+public class Var1 {
+
+    public static void main(String[] args) {
+        int num1;
+        String str1;
+        num1 = 10;
+        str1 = "Hello World!";
+        System.out.println(num1);
+        System.out.println(str1);
+
+        num1 = 20;  //代入
+        System.out.println(num1);
+
+        int num2 = 30;  //int型その2
+        System.out.println(num2);
+
+        final int MIN_NUMBER = 10; //定数
+        System.out.println(MIN_NUMBER);
+    }
+}
