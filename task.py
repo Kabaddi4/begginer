d58899c799a7c1948825315c862d5a52b5bf1b9b
555Message <zaku596@yahoo.co.jp>
2022-12-05 15:14:56 +0900

    create weak,task

---
+++ task.py
@@ -0,0 +1,6 @@
+def plus(num1, num2):
+    return num1 * num2
+
+a, b = map(int, input().split())
+
+ans = plus(a, b)
---
+++ weak.py
@@ -0,0 +1,10 @@
+def plus(num1, num2):
+    return num1 + num2
+
+
+arr = [3, 5, 14, 22]
+
+print(arr[2])   #表示
+
+ans = plus(arr[0], arr[1])  #引数を渡して関数呼び出し
+print(ans)  #処理後の値の表示
