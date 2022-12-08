f65a6f5d387d4415b80e808adb961cdd197f77ef
555Message <zaku596@yahoo.co.jp>
2022-12-08 17:41:35 +0900

    update override

--- override.py
+++ override.py
@@ -5,13 +5,16 @@ class Phone:
     def buy(self):
         print(self.model + "を購入した")
 
-class CellPhone(Phone):
+class CellPhone(Phone):     #継承
     def evol(self):
         print("携帯電話に進化した")
+
+    def buy(self):
+        print(self.model + "をネットで購入した")    #オーバーライド
     
-phone = Phone("電話")
+phone = Phone("電話")   #親クラスのインスタンス
 phone.buy()
 
-cellphone = CellPhone("携帯電話")
+cellphone = CellPhone("携帯電話")      #子クラスのインスタンス
 cellphone.evol()
 cellphone.buy()
