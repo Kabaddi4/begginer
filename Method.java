1f3d46288da77000444400243c26fbed25d5f8d2
555Message <zaku596@yahoo.co.jp>
2022-12-10 17:55:37 +0900

    create Method.java

---
+++ Method.java
@@ -0,0 +1,9 @@
+public class Method {
+    public static void main(String[] args) {
+        buyPhone();
+    }
+
+    public static void buyPhone() {
+        System.out.println("buy smartphone");
+    }
+}
