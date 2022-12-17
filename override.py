4011e940c8f61ff47b613068505c246e2859c776
555Message <zaku596@yahoo.co.jp>
2022-12-17 16:19:13 +0900

    update override.py

--- override.py
+++ override.py
@@ -12,9 +12,13 @@ class CellPhone(Phone):     #継承
     def buy(self):
         print(self.model + "をネットで購入した")    #オーバーライド
     
+    def __charge(self):     #プライベートメソッド
+        print("充電中")
+    
 phone = Phone("電話")   #親クラスのインスタンス
 phone.buy()
 
 cellphone = CellPhone("携帯電話")      #子クラスのインスタンス
 cellphone.evol()
 cellphone.buy()
+cellphone._CellPhone__charge()      #_Class__private method()
