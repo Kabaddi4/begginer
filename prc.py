7b58ae32a3a0680dc5fecdd8ad57540afe24c412
555Message <zaku596@yahoo.co.jp>
2022-12-19 16:40:16 +0900

    update prc.py

--- prc.py
+++ prc.py
@@ -1,6 +1,17 @@
-def keisan(num):
-    return int(num) * 1.4   #引数をintで囲んでから行う
+def equal(num_a, num_d, num_n):
+    return (num_a + num_d) * num_n / 2
 
-output = input("数字を入力してください：")
-ans = keisan(output)
-print(ans)
+def arr_equal(num_a, num_d, num_n):
+    return (num_a + num_d) * num_n / 2
+
+arr = [1, 5, 9]     
+a = arr[0]
+judge_1 = arr[1]
+d = arr[-1]
+n = len(arr)    #要素数
+
+if judge_1 - a == d - judge_1:
+    ans = arr_equal(a, d, n)
+    print(ans)
+else:
+    print("null")
--- suuretu.py
+++ suuretu.py
@@ -3,8 +3,10 @@ arr = [1, 5, 9]
 a = arr[0]
 middle = arr[1]
 d = arr[2]
+n = len(arr)
 
 if middle - a == d - middle:
     print("equal")
+    print(n)
 else:
     print("null")
